0ef5f0cab5128893ced252d641258e0f7e4fa7e2
theonly112 <theonly112@gmail.com>
2020-09-20 19:05:39 +0200

    some shield lab stuff.

--- Core/Game/Tasks/ShieldLab.cs
+++ Core/Game/Tasks/ShieldLab.cs
@@ -18,6 +18,12 @@ namespace autoplaysharp.Game.Tasks
             }
             await OpenMenu().ConfigureAwait(false);
             Game.Click(UIds.MAIN_MENU_SHIELD_LAB_BUTTON);
+
+            await Task.Delay(1000);
+
+            Game.Click(UIds.SHIELD_LAB_COLLECT_ANTIMATTER);
+
+            // TODO: stuff...
         }
     }
 }
--- Core/ui/shield_lab.json.cs
+++ Core/ui/shield_lab.json.cs
@@ -4,4 +4,12 @@ partial class UIds
 	/// SHIELD_LAB_MENU_HEADER
 	/// </summary>
 	public const string SHIELD_LAB_MENU_HEADER="SHIELD_LAB_MENU_HEADER";
+	/// <summary>
+	/// SHIELD_LAB_COLLECT_ANTIMATTER
+	/// </summary>
+	public const string SHIELD_LAB_COLLECT_ANTIMATTER="SHIELD_LAB_COLLECT_ANTIMATTER";
+	/// <summary>
+	/// SHIELD_LAB_LEFT_ITEM
+	/// </summary>
+	public const string SHIELD_LAB_LEFT_ITEM="SHIELD_LAB_LEFT_ITEM";
 }
