80fc12886d214c1e36bdb71e16dd069fcb91b0e9
theonly112 <theonly112@gmail.com>
2020-09-20 13:11:40 +0200

    Auto scroll to end of log

---
+++ App.Wpf/UI/Log/AutoScrollBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace autoplaysharp.App.UI.Log
+{
+    internal class AutoScrollBehavior
+    {
+
+        public static bool GetEnableAutoScroll(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(EnableAutoScrollProperty);
+        }
+
+        public static void SetEnableAutoScroll(DependencyObject obj, bool value)
+        {
+            obj.SetValue(EnableAutoScrollProperty, value);
+        }
+
+        public static readonly DependencyProperty EnableAutoScrollProperty =
+        DependencyProperty.RegisterAttached("EnableAutoScroll", typeof(bool), typeof(AutoScrollBehavior), new PropertyMetadata(false, AutoScrollToEndPropertyChanged));
+
+        private static void AutoScrollToEndPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextBox textbox && e.NewValue is bool mustAutoScroll && mustAutoScroll)
+            {
+                textbox.TextChanged += (s, ee) => textbox.ScrollToEnd();
+            }
+        }
+    }
+}
