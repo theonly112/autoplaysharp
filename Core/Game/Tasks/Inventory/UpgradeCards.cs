ae398f15c63a252025a1e8a49851b382926bdcad
theonly112 <theonly112@gmail.com>
2020-10-13 20:41:02 +0200

    improve comic card upgrade task

--- Core/Game/Tasks/Inventory/UpgradeCards.cs
+++ Core/Game/Tasks/Inventory/UpgradeCards.cs
@@ -21,17 +21,25 @@ namespace autoplaysharp.Core.Game.Tasks.Inventory
 
             await ClickWhenVisible(UIds.INVENTORY_TAB_CARDS);
             await ClickWhenVisible(UIds.INVENTORY_TAB_CARDS_UPGRADE_ALL);
+            await Task.Delay(1000);
             for(int x = 0; x < 3; x++)
             {
-              
                 var text = Game.GetText(Repository[UIds.INVENTORY_TAB_CARDS_UPGRADE_AMOUNT_DYN, x, 0]);
                 int.TryParse(text, out var num);
+                Logger.LogDebug($"Found {num} cards of {x} type.");
                 if(num > 6)
                 {
                     Game.Click(Repository[UIds.INVENTORY_TAB_CARDS_UPGRADE_TYPE_DYN, x, 0]);
                     await ClickWhenVisible(UIds.INVENTORY_TAB_CARDS_UPGRADE_SELECT);
                     await ClickWhenVisible(UIds.INVENTORY_TAB_CARDS_UPGRADE_BUTTON);
                     await ClickWhenVisible(UIds.INVENTORY_TAB_CARDS_UPGRADE_RESULT_OK);
+                    // TODO: is this only visible when more cards can be upgraded?
+                    await ClickWhenVisible(UIds.INVENTORY_TAB_CARDS_UPGRADE_ALL_CANCEL);
+                    await HandleHeroicQuestNotice();
+                }
+                else
+                {
+                    Logger.LogDebug($"Not enough cards in of {x}. type.");
                 }
             }
       
