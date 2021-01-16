ca221caf713695a81c31e1592c19f2cede23d1ee
theonly112 <theonly112@gmail.com>
2021-01-16 09:27:12 +0100

    Fix publish (hopefully)

--- Cleanup.ps1
+++ Cleanup.ps1
@@ -1,7 +1,5 @@
-Get-ChildItem * -Include *.pdb -Recurse | Remove-Item
-
-$dirs = $data = @('dll','x86','cs','de','es','fr','it','ja','ko','pl','pt-BR','ru','tr','zh-Hans','zh-Hant')
+$dirs = $data = @('dll\x86','x86','cs','de','es','fr','it','ja','ko','pl','pt-BR','ru','tr','zh-Hans','zh-Hant')
 foreach($d in $dirs)
 {
-	Remove-Item .\App.Wpf\bin\Release\$d -Recurse -ErrorAction SilentlyContinue
+	Remove-Item .\App.Wpf\bin\Release\win10-x64\publish\$d -Recurse -ErrorAction SilentlyContinue
 }
