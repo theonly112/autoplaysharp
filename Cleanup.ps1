7c4fd27bffb04e499246e5bbb11e30ef0085ba0d
theonly112 <theonly112@gmail.com>
2021-09-19 19:22:34 +0200

    update github actions

--- Cleanup.ps1
+++
@@ -1,5 +0,0 @@
-$dirs = $data = @('dll\x86','x86','cs','de','es','fr','it','ja','ko','pl','pt-BR','ru','tr','zh-Hans','zh-Hant')
-foreach($d in $dirs)
-{
-	Remove-Item .\App.Wpf\bin\Release\win10-x64\publish\$d -Recurse -ErrorAction SilentlyContinue
-}
